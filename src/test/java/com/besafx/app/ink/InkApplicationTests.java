a8ea30d9172df6fe38e2bfc13e903fd3e9351ab0
BesaFX <islamhaker@gmail.com>
2020-02-07 11:49:19 +0200

    Starting new product with the Name Of Alla.

---
+++ src/test/java/com/besafx/app/ink/InkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besafx.app.ink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
